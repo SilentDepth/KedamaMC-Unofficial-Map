6336a68d532830def48dabdbb5848eb1301c5ac8
SilentDepth <silentdepth@outlook.com>
2017-03-13 17:49:07 +0800

    an anyhow working version

--- src/stats-report/index.js
+++ src/stats-report/index.js
@@ -2,70 +2,77 @@
 
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
 const ORIGIN = 'https://stats.kedamamc.com';
 
-function getJSON(url) {
+function getJSON(url, file) {
   return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      const statusCode = res.statusCode;
-      const contentType = res.headers['content-type'];
+    https.get(url, response => {
+      const statusCode = response.statusCode;
+      const contentType = response.headers['content-type'];
       if (statusCode !== 200) {
         console.error(`Request Failed. (Code: ${statusCode})`);
-        reject(res);
+        reject(response);
       } else if (contentType !== 'application/json') {
         console.error(`Invalid content-type. Received ${contentType}`);
-        reject(res);
+        reject(response);
         return;
       }
 
       let raw = '';
-      res.setEncoding('utf8');
-      res.on('data', chunk => raw += chunk);
-      res.on('end', () => {
+      response.setEncoding('utf8');
+      response.pipe(fs.createWriteStream('./data/' + file || path.basename(url)));
+      response.on('data', chunk => raw += chunk);
+      response.on('end', () => {
         try {
           resolve(JSON.parse(raw));
         } catch (e) {
           console.error(e.message);
         }
       });
+    }).setTimeout(5000, () => {
+      console.warn(`Timeout for ${url}, will retry later`);
+      reject('timeout');
     }).on('error', e => {
       console.error(`Got error: ${e.message}`);
     });
-  }).catch(res => {
-    res.resume();
+  }).catch(response => {
+    response.resume();
   });
 }
 
 function short(uuid) {
-  return uuid.replace('-', '');
+  return uuid.replace(/-/g, '');
 }
 
-function one(promise) {
-  return Promise.all([
-    promise,
-    new Promise(resolve => {
-      setTimeout(resolve, 1000);
-    }),
-  ]);
+async function throttle(func, ...args) {
+  try {
+    const values = await Promise.all([
+      func(...args),
+      new Promise(resolve => setTimeout(resolve, 1000)),
+    ]);
+    return values[0];
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
-function download(url, file) {
-  return new Promise((resolve, reject) => {
-    https.get(`${ORIGIN}/players.json`, res => {
-      const statusCode = res.statusCode;
-      if (statusCode !== 200) {
-        console.error(statusCode);
-        reject();
-        return;
+(async function () {
+  const players = await getJSON(ORIGIN + '/players.json');
+  console.log(`${players.players.length} players in total`);
+
+  for (const uuid of players.players) {
+    console.log(`Downloading ${uuid}...`);
+    try {
+      await throttle(getJSON, `${ORIGIN}/${short(uuid)}/stats.json`, uuid + '.json');
+    } catch (e) {
+      console.log(e, '-', e.message);
+      if (e.message || e === 'timeout') {
+        players.players.push(uuid);
       }
+    }
+  }
 
-      const target = fs.createWriteStream('./data/f4600964-9ff0-4a52-a958-31b7b9b1330a.json');
-      res.pipe(target, {end: false});
-      res.on('end', () => {
-        console.log('end');
-        resolve();
-      });
-    });
-  });
-}
+  console.log('All done');
+}());
